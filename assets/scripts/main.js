05ef3604df16e2fd38687108054e0b1ea9b9efee
Usman-Kashmiri <usmankashmiri378@gmail.com>
2021-06-15 12:54:28 +0500

    added monitors page

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,11 +4,24 @@ $(document).ready(function() {
         $('.menu').toggleClass("active");
         $('.menu-btn .menu').toggleClass('active');
     });
-
     $('#accessories-btn').click(function() {
         $('#accessories-content').toggle();
     })
     $('#products-btn').click(function() {
         $('#products-content').toggle();
     })
+
+    $('body').click(function() {
+        $('#accessories-content').hide();
+    });
+    $('#accessories-btn').click(function(event) {
+        event.stopPropagation();
+    });
+
+    $('body').click(function() {
+        $('#products-content').hide();
+    });
+    $('#products-btn').click(function(event) {
+        event.stopPropagation();
+    });
 })
