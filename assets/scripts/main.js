29604161bb129d95914eadf928474ec00d38b49b
Usman-Kashmiri <usmankashmiri378@gmail.com>
2021-06-16 20:56:47 +0500

    Made Nav-bar stick on top on scroll down

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,29 @@
 $(document).ready(function() {
+
+    // Menu toggle btn
     $('.menu-btn').click(function() {
         $('ul.toggle').toggleClass('active');
         $('.menu').toggleClass("active");
         $('.menu-btn .menu').toggleClass('active');
     });
+    // Current Page effect
+    $('ul li a').click(function() {
+        $('li a').removeClass("current");
+        $(this).addClass("current");
+    });
+
+    // Navbar onScroll effect
+    var stickyOffset = $('.nav-bar').offset().top;
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() >= stickyOffset) {
+            $('.nav-bar').addClass('fixed');
+        } else {
+            $('.nav-bar').removeClass('fixed');
+        }
+    });
+
+    // Drop-down
     $('#accessories-btn').click(function() {
         $('#accessories-content').toggle();
     })
